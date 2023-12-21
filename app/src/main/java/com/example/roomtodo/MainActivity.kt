7f9958bf057792cb2e1bd51231e4f8459abda920
omi-72 <jannat.omi722@gmail.com>
2023-12-21 14:11:38 +0600

    create Room project and create show task activity , add layout,update layout UI

---
+++ app/src/main/java/com/example/roomtodo/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.roomtodo
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.example.roomtodo.databinding.ActivityMainBinding
+import com.example.roomtodo.utils.setupDialog
+
+class MainActivity : AppCompatActivity() {
+
+    private val mainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val addTaskDialog: Dialog by lazy {
+        Dialog(this, R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.add_task_dialog)
+        }
+    }
+
+    private val updateTaskDialog: Dialog by lazy {
+        Dialog(this, R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.update_task_dialog)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(mainBinding.root)
+
+        val addCloseImg = addTaskDialog.findViewById<ImageView>(R.id.closeImg)
+        val updateCloseImg = updateTaskDialog.findViewById<ImageView>(R.id.closeImg)
+
+        addCloseImg.setOnClickListener { addTaskDialog.dismiss() }
+        updateCloseImg.setOnClickListener { updateTaskDialog.dismiss() }
+
+        mainBinding.addTaskFABtn.setOnClickListener {
+            addTaskDialog.show()
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/roomtodo/utils/utils.kt
@@ -0,0 +1,16 @@
+package com.example.roomtodo.utils
+
+import android.app.Dialog
+import android.widget.LinearLayout
+
+fun Dialog.setupDialog(layoutResId : Int){
+    setContentView(layoutResId)
+
+    window!!.setLayout(
+        LinearLayout.LayoutParams.MATCH_PARENT,
+        LinearLayout.LayoutParams.WRAP_CONTENT
+    )
+
+    setCancelable(false)
+}
+
